ef881991b9a63cf485d35262e71b5950de100c80
baKon <mike@geoskygps.com>
2022-07-15 22:50:43 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MyApp from "./containers/MyApp";
 
-ReactDOM.render(<MyApp />, document.getElementById("root"));
+const routing = (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+          <MyApp />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(
+  routing, document.getElementById("root")
+);
